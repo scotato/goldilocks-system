b9853279d519ed16065889c152601794ec6f4b91
Scott Dodge <scott@goldilocks.design>
2020-03-03 11:22:41 -0600

    add styled-components, theme, button story experiments

--- src/index.tsx
+++ src/index.tsx
@@ -4,3 +4,11 @@ import * as React from 'react';
 export const Thing = () => {
   return <div>the snozzberries taste like snozzberries</div>;
 };
+
+export const GlobalStyle = () => {
+  return <div>placeholder</div>;
+};
+
+// export const Button = () => <button>hello</button>
+export { default as Button } from './components/Button';
+export { default as theme } from './theme';
